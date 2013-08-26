1e4d018142913bbb584dfa06d44dbebcaa539175
karuppiah sundaram <s.karupp@gmail.com>
2013-08-26 11:01:37 +0530

    My first java project into GIT repository

---
+++ karuppiah/src/com/mine/MyFirst.java
@@ -0,0 +1,13 @@
+package com.mine;
+
+public class MyFirst {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello java world!");
+	}
+
+}
